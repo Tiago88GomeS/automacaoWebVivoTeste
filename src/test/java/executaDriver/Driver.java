3ba4932abfaa56e71d87c0c2ebdbb1a2e9fb1124
Tiago Gomes <tiagoogomes1988@gmail.com>
2022-06-02 19:28:02 -0300

    Initial commit

---
+++ src/test/java/executaDriver/Driver.java
@@ -0,0 +1,41 @@
+package executaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    WebDriver driver;
+
+    public Driver(String navegador){
+        switch (navegador){
+            case "ie":
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
