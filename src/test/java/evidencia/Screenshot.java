2f94790938cfe23dc48b672a3be85c542598ddaf
Tiago Gomes <tiagoogomes1988@gmail.com>
2022-06-03 09:02:00 -0300

    Adicionando o comando para tirar as evidencias do teste

--- src/test/java/evidencia/Screenshot.java
+++ src/test/java/evidencia/Screenshot.java
@@ -14,7 +14,7 @@ public class Screenshot {
         try {
             FileUtils.copyFile(screenshot, new File(arquivo));
         }catch(Exception e){
-            System.out.println("Houve problema ao copiar o arquivo para pasta: " + e.getMessage());
+            System.out.println("Houve algum problema  ao copiar o arquivo para pasta: " + e.getMessage());
         }
     }
 }
